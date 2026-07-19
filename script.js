0d34afce34f5f265cad478e2f82c0c17275a0343
Kendamohamedadel <akarish4444@gmail.com>
2026-07-19 10:42:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const title = document.querySelector(".hero h1");
+
+    title.style.opacity = "0";
+    title.style.transform = "translateY(-30px)";
+
+    setTimeout(() => {
+        title.style.transition = "1s";
+        title.style.opacity = "1";
+        title.style.transform = "translateY(0)";
+    }, 300);
+
+});
