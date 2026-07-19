ac41b55500ab7bee844c9e6de8d6a4fd29061439
Kendamohamedadel <akarish4444@gmail.com>
2026-07-19 12:56:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,132 @@
-document.addEventListener("DOMContentLoaded", function () {
+// =========================
+// Image Gallery Popup
+// =========================
 
-    const title = document.querySelector(".hero h1");
+let images = document.querySelectorAll(".gallery img");
 
-    title.style.opacity = "0";
-    title.style.transform = "translateY(-30px)";
+let popup = document.getElementById("popup");
+let popupImg = document.getElementById("popup-img");
+let closeBtn = document.getElementById("close");
 
-    setTimeout(() => {
-        title.style.transition = "1s";
-        title.style.opacity = "1";
-        title.style.transform = "translateY(0)";
-    }, 300);
+
+images.forEach(image => {
+
+    image.onclick = function(){
+
+        popup.style.display = "flex";
+
+        popupImg.src = this.src;
+
+    }
+
+});
+
+
+closeBtn.onclick = function(){
+
+    popup.style.display = "none";
+
+};
+
+
+
+popup.onclick = function(e){
+
+    if(e.target === popup){
+
+        popup.style.display="none";
+
+    }
+
+};
+
+
+
+
+// =========================
+// Back To Top Button
+// =========================
+
+let topBtn = document.getElementById("topBtn");
+
+
+window.onscroll = function(){
+
+    if(window.scrollY > 500){
+
+        topBtn.style.display="flex";
+
+    }
+
+    else{
+
+        topBtn.style.display="none";
+
+    }
+
+};
+
+
+
+
+// =========================
+// Scroll Animation
+// =========================
+
+const sections = document.querySelectorAll("section");
+
+
+window.addEventListener("scroll",()=>{
+
+
+    sections.forEach(section=>{
+
+
+        let position = section.getBoundingClientRect().top;
+
+
+        let screen = window.innerHeight;
+
+
+        if(position < screen - 100){
+
+            section.style.opacity="1";
+
+            section.style.transform="translateY(0)";
+
+        }
+
+
+    });
+
+
+});
+
+
+
+
+// =========================
+// Navbar Background Change
+// =========================
+
+
+let nav = document.querySelector("nav");
+
+
+window.addEventListener("scroll",()=>{
+
+
+    if(window.scrollY > 100){
+
+        nav.style.background="rgba(0,0,0,.95)";
+
+    }
+
+    else{
+
+        nav.style.background="rgba(0,0,0,.75)";
+
+    }
+
 
 });
